fe4947eafe512004736985dee7a4a777bc83136c
João Paulo M Ragazzo <joao_ragazzo@hotmail.com>
2022-03-23 02:56:25 -0300

    Dando novas funcionalidades para o botão Info e Mensagens

---
+++ scripts/info.js
@@ -0,0 +1,7 @@
+var btnAwnsers = document.getElementById("btn-awnsers");
+
+btnAwnsers.addEventListener("click", respostas)
+
+function respostas(){
+    window.location.href = "login.html";
+}
--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,12 @@
 var btnSend = document.getElementById("send-button");
+var btnInfo = document.getElementById("info-button")
 
-btnSend.addEventListener("click", teste);
 
-function teste(){
+btnSend.addEventListener("click", enviar);
+btnInfo.addEventListener("click", info)
+
+
+function enviar(){
     
     let params = {
         active: true,
@@ -13,8 +17,13 @@ function teste(){
 
     function gotTabs(tabs) {
         let msg = {
-            txt: "hello world!"
+            txt: "Olá! Não estamos atendendo no momento. Nos chama amanhã, as oito e sete!"
         }
         chrome.tabs.sendMessage(tabs[0].id, msg);
     }
+}
+
+function info(){
+    window.location.href = "info.html";
+    return 0
 }
